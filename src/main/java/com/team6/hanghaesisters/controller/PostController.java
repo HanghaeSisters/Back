10fd69efd88507bd7864c4e4d24c400d09f907e5
starMinK <rbals040329@gmail.com>
2022-12-19 21:57:34 +0900

    jwt security key 에러 수정

--- src/main/java/com/team6/hanghaesisters/controller/PostController.java
+++ src/main/java/com/team6/hanghaesisters/controller/PostController.java
@@ -6,12 +6,14 @@ import com.team6.hanghaesisters.dto.PostResponseDto;
 import com.team6.hanghaesisters.entity.Post;
 import com.team6.hanghaesisters.service.PostService;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
 @RequiredArgsConstructor
+@RestController
 @RequestMapping("/api/post")
 @Slf4j
 public class PostController {
@@ -19,27 +21,32 @@ public class PostController {
     private final PostService postService;
 
     //글 작성
-    @PostMapping
+    @PostMapping("")
     public PostResponseDto createPost(@RequestBody PostRequestDto postRequestDto, HttpServletRequest request){
         log.info("controller-createPost메소드 실행 완");
         return postService.create(postRequestDto, request);
     }
 
     //글 수정
-    @PutMapping("{id}")
+    @PutMapping("/{id}")
     public PostResponseDto readOnePost(@PathVariable Long id, HttpServletRequest request) {
         return postService.readOne(id, request);
     }
 
     //글 조회
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     public PostResponseDto updatePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto, HttpServletRequest request){
         return postService.update(id, postRequestDto, request);
     }
     //글 삭제
-    @DeleteMapping("{id}")
+    @DeleteMapping("/{id}")
     public MsgResponseDto deletePost(@PathVariable Long id, HttpServletRequest request){
         return postService.delete(id, request);
     }
 
 }
+
+
+
+
+
