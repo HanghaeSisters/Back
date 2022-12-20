8933c31d3d48c7d2e1933070406cc4f5cf7ab168
rara-dev <kksr1219@gmail.com>
2022-12-20 23:26:40 +0900

    🔨Comment: 반환타입 변경에 따른 코드 수정

--- src/main/java/com/team6/hanghaesisters/controller/UserController.java
+++ src/main/java/com/team6/hanghaesisters/controller/UserController.java
@@ -3,18 +3,15 @@ package com.team6.hanghaesisters.controller;
 import com.team6.hanghaesisters.dto.MsgResponseDto;
 import com.team6.hanghaesisters.dto.UserDto;
 import com.team6.hanghaesisters.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController //Json형태로 객체 데이터를 반환하는데 사용됨
@@ -22,44 +19,21 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor //생성자를 만들어야 하는 번거로움을 없애줌
 public class UserController {
 
-    private final UserService userService; //의존성주입
+	private final UserService userService; //의존성주입
 
-    @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody @Valid UserDto.SignupReqDto reqDto) {
-        userService.signup(reqDto);
-        return ResponseEntity.ok(new MsgResponseDto("회원가입 되었습니다.", HttpStatus.OK.value()));
-    }
+	@PostMapping("/signup")
+	public MsgResponseDto signup(@RequestBody @Valid UserDto.SignupReqDto reqDto) {
+		return userService.signup(reqDto);
+	}
 
-    @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody UserDto.LoginReqDto reqDto){
+	@PostMapping("/login")
+	public MsgResponseDto login(@RequestBody UserDto.LoginReqDto reqDto, HttpServletResponse httpServletResponse) {
+		return userService.login(reqDto, httpServletResponse);
+	}
 
-        // 생성된 토큰을 헤더에 담습니다.
-        String accessToken = userService.login(reqDto);
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add(HttpHeaders.AUTHORIZATION, accessToken);
-
-        // body, header, status
-        return new ResponseEntity<>(new MsgResponseDto("로그인 되었습니다.", HttpStatus.OK.value()), httpHeaders, HttpStatus.OK);
-    }
-
-    @GetMapping("/idcheck")
-    public ResponseEntity<?> checkIdDuplication(@RequestParam("username") String username) {
-        System.out.println(username);
-
-        if (userService.existsByUsername(username) == true) {
-            return ResponseEntity.ok(new MsgResponseDto("이미 존재하는 아이디입니다.", HttpStatus.NOT_FOUND.value()));
-        } else {
-            return ResponseEntity.ok(new MsgResponseDto("사용 가능한 아이디입니다.", HttpStatus.OK.value()));
-        }
-    }
-
-//    @GetMapping("/logout")
-//    public String logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        if (auth != null) {
-//            new SecurityContextLogoutHandler().logout(request, response, auth);
-//        }
-//        return "redirect:/";
-//    }
+	@GetMapping("/idcheck")
+	public MsgResponseDto checkIdDuplication(@RequestParam("username") String username) {
+		return userService.checkIdDuplication(username);
+	}
 }
 
