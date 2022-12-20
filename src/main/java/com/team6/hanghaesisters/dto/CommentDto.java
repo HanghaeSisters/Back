e379bca27f420b8d7c3088e78d8c5a8ba980651a
rara-dev <kksr1219@gmail.com>
2022-12-20 13:52:18 +0900

    Comment 기능 구현

--- src/main/java/com/team6/hanghaesisters/dto/CommentDto.java
+++ src/main/java/com/team6/hanghaesisters/dto/CommentDto.java
@@ -3,12 +3,11 @@ package com.team6.hanghaesisters.dto;
 import com.team6.hanghaesisters.entity.Comment;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import java.util.List;
 
 public class CommentDto {
 
 	public record RequestDto(@NotBlank(message = "내용을 입력해주세요.")
-							 @Size(min = 1, max = 200, message = "댓글은 200자로 입력해주세요.")
+							 @Size(min = 1, max = 200, message = "댓글은 200자 이하로 입력해주세요.")
 							 String content) {
 	}
 
@@ -17,9 +16,5 @@ public class CommentDto {
 		public ResponseDto(Comment comment, String username) {
 			this(comment.getId(), comment.getContent(), username);
 		}
-	}
-
-	public record AllResponseDto(List<ResponseDto> commentList) {
-
 	}
 }
