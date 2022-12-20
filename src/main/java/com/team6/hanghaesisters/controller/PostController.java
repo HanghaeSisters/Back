6b5131438b1804d6691c09a45c57afd9a25bd20a
starMinK <rbals040329@gmail.com>
2022-12-20 15:10:37 +0900

    post 코드 수정

--- src/main/java/com/team6/hanghaesisters/controller/PostController.java
+++ src/main/java/com/team6/hanghaesisters/controller/PostController.java
@@ -1,17 +1,15 @@
 package com.team6.hanghaesisters.controller;
 
-import ch.qos.logback.core.model.Model;
 import com.team6.hanghaesisters.dto.MsgResponseDto;
 import com.team6.hanghaesisters.dto.PostRequestDto;
 import com.team6.hanghaesisters.dto.PostResponseDto;
 import com.team6.hanghaesisters.dto.PostSampleResponseDto;
-import com.team6.hanghaesisters.entity.Post;
+import com.team6.hanghaesisters.security.UserDetailsImpl;
 import com.team6.hanghaesisters.service.PostService;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,31 +19,33 @@ import java.util.List;
 @RequestMapping("/api/post")
 @Slf4j
 public class PostController {
-/*
     private final PostService postService;
 
     //글 작성
     @PostMapping("")
-    public PostResponseDto createPost(@RequestBody PostRequestDto postRequestDto, HttpServletRequest request){
-        log.info("controller-createPost메소드 실행 완");
-        return postService.create(postRequestDto, request);
+    public PostResponseDto createPost(@RequestBody PostRequestDto postRequestDto,
+                                      @AuthenticationPrincipal UserDetailsImpl userDetails){
+        log.info("UserDetails:" + userDetails);
+        return postService.create(postRequestDto, userDetails.getUser());
     }
 
     //글 조회
     @GetMapping("/{id}")
-    public PostResponseDto readOnePost(@PathVariable Long id, HttpServletRequest request) {
-        return postService.readOne(id, request);
+    public PostResponseDto readOnePost(@PathVariable Long id,
+                                       @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return postService.readOne(id, userDetails.getUser());
     }
 
     //글 수정
     @PutMapping("/{id}")
-    public PostResponseDto updatePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto, HttpServletRequest request){
-        return postService.update(id, postRequestDto, request);
+    public PostResponseDto updatePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto,
+                                      @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return postService.update(id, postRequestDto, userDetails.getUser());
     }
     //글 삭제
     @DeleteMapping("/{id}")
-    public MsgResponseDto deletePost(@PathVariable Long id, HttpServletRequest request){
-        return postService.delete(id, request);
+    public MsgResponseDto deletePost(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return postService.delete(id, userDetails.getUser());
     }
 
 //    @RequestParam
@@ -54,7 +54,6 @@ public class PostController {
         return postService.readByCategory(category);
     }
 
-    */
 }
 
 
--- src/main/java/com/team6/hanghaesisters/service/PostService.java
+++ src/main/java/com/team6/hanghaesisters/service/PostService.java
@@ -6,14 +6,15 @@ import com.team6.hanghaesisters.dto.PostResponseDto;
 import com.team6.hanghaesisters.dto.PostSampleResponseDto;
 import com.team6.hanghaesisters.entity.Post;
 import com.team6.hanghaesisters.entity.User;
-//import com.team6.hanghaesisters.jwt.JwtUtil;
+import com.team6.hanghaesisters.exception.CustomException;
+import com.team6.hanghaesisters.exception.ErrorCode;
 import com.team6.hanghaesisters.repository.PostRepository;
 import com.team6.hanghaesisters.repository.UserRepository;
-import io.jsonwebtoken.Claims;
-import jakarta.servlet.http.HttpServletRequest;
+import com.team6.hanghaesisters.util.ExceptionFunctions;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -23,129 +24,45 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 public class PostService {
-/*
     private final PostRepository postRepository;
-    private final UserRepository userRepository;
-    private final JwtUtil jwtUtil;
+    private final ExceptionFunctions exceptionFunctions;
 
     @Transactional
-    public PostResponseDto create(PostRequestDto postRequestDto, HttpServletRequest request) {
+    public PostResponseDto create(PostRequestDto postRequestDto, User user) {
         log.info("PostService create() 실행 완");
-        String token = jwtUtil.resolveToken(request);
-        Claims claims;
 
-        // 토큰이 있는 경우에만 관심상품 추가 가능
-        if (token != null) {
-            if (jwtUtil.validateToken(token)) {
-                // 토큰에서 사용자 정보 가져오기
-                claims = jwtUtil.getUserInfoFromToken(token);
-            } else {
-                throw new CustomException(ErrorCode.INVALID_TOKEN);
-            }
-
-            // 토큰에서 가져온 사용자 정보를 사용하여 DB 조회
-            User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
-                    () -> new CustomException(ErrorCode.USER_NOT_FOUND)
-            );
-
-            // 요청받은 DTO 로 DB에 저장할 객체 만들기
-            Post post = postRepository.saveAndFlush(new Post(postRequestDto, user.getUsername()));
-
-            return new PostResponseDto(post);
-        } else {
-            return null;
-        }
+        Post post = postRepository.saveAndFlush(new Post(postRequestDto, user.getUsername()));
+        return new PostResponseDto(post);
     }
 
     @Transactional
-    public PostResponseDto readOne(Long id, HttpServletRequest request) {
-        // Request에서 Token 가져오기
-        String token = jwtUtil.resolveToken(request);
-        Claims claims;
-
-        // 토큰이 있는 경우에만 관심상품 조회 가능
-        if (token != null) {
-            // Token 검증
-            if (jwtUtil.validateToken(token)) {
-                claims = jwtUtil.getUserInfoFromToken(token);
-            } else {
-                throw new CustomException(ErrorCode.INVALID_TOKEN);
-            }
-
-            User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
-                    () -> new CustomException(ErrorCode.USER_NOT_FOUND)
-            );
-
-            Post post = postRepository.findById(id).orElseThrow(
-                    () -> new CustomException(ErrorCode.NOT_FOUND_POST)
-            );
-
-            return new PostResponseDto(post);
-
-        } else {
-            return null;
-        }
+    public PostResponseDto readOne(Long id, User user) {
+        Post post = postRepository.findById(id).orElseThrow(
+                () -> new CustomException(ErrorCode.NOT_FOUND_POST)
+        );
+        return new PostResponseDto(post);
     }
 
     @Transactional
-    public PostResponseDto update(Long id, PostRequestDto postRequestDto,
-                                  HttpServletRequest request) {
-        // Request에서 Token 가져오기
-        String token = jwtUtil.resolveToken(request);
-        Claims claims;
+    public PostResponseDto update(Long id, PostRequestDto postRequestDto, User user) {
+        exceptionFunctions.checkPost(id);
 
-        // 토큰이 있는 경우에만 관심상품 조회 가능
-        if (token != null) {
-            // Token 검증
-            if (jwtUtil.validateToken(token)) {
-                claims = jwtUtil.getUserInfoFromToken(token);
-            } else {
-                throw new CustomException(ErrorCode.INVALID_TOKEN);
-            }
-
-            // 토큰에서 가져온 사용자 정보를 사용하여 DB 조회
-            User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
-                    () -> new CustomException(ErrorCode.USER_NOT_FOUND)
-            );
-
-            Post post = postRepository.findByIdAndUsername(id, user.getUsername()).orElseThrow(
-                    () -> new CustomException(ErrorCode.UNAVAILABLE_MODIFICATION)
-            );
-            post.update(postRequestDto);
-            return new PostResponseDto(post);
-        } else {
-            return null;
-        }
+        Post post = postRepository.findByIdAndUsername(id, user.getUsername()).orElseThrow(
+                () -> new CustomException(ErrorCode.UNAVAILABLE_MODIFICATION)
+        );
+        post.update(postRequestDto);
+        return new PostResponseDto(post);
     }
 
     @Transactional
-    public MsgResponseDto delete(Long id, HttpServletRequest request) {
-        // Request에서 Token 가져오기
-        String token = jwtUtil.resolveToken(request);
-        Claims claims;
+    public MsgResponseDto delete(Long id, User user) {
+        exceptionFunctions.checkPost(id);
 
-        // 토큰이 있는 경우에만 관심상품 조회 가능
-        if (token != null) {
-            // Token 검증
-            if (jwtUtil.validateToken(token)) {
-                claims = jwtUtil.getUserInfoFromToken(token);
-            } else {
-                throw new CustomException(ErrorCode.INVALID_TOKEN);
-            }
+        Post post = postRepository.getReferenceById(id);
+        exceptionFunctions.checkOwner(post, user.getUsername());
 
-            // 토큰에서 가져온 사용자 정보를 사용하여 DB 조회
-            User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
-                    () -> new CustomException(ErrorCode.USER_NOT_FOUND)
-            );
-
-            Post post = postRepository.findByIdAndUsername(id, user.getUsername()).orElseThrow(
-                    () -> new CustomException(ErrorCode.UNAVAILABLE_MODIFICATION)
-            );
-            postRepository.deleteByIdAndUsername(id, user.getUsername());
-            return new MsgResponseDto("게시글 삭제 성공!!", 200);
-        } else {
-            return new MsgResponseDto("token이 존재하지 않습니다.", 404);
-        }
+        postRepository.deleteByIdAndUsername(id, user.getUsername());
+        return new MsgResponseDto("게시글 삭제 성공!!", HttpStatus.OK.value());
     }
 
     @Transactional
@@ -165,5 +82,4 @@ public class PostService {
 
         return postSampleResponseDtoList;
     }
- */
 }
