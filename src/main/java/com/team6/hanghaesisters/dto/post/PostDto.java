2845f8030805c16598b9fb5eef5fc580eaf48580
civilcoy <joungcb06@gmail.com>
2022-12-21 00:59:27 +0900

    POSTDTO >  detailResDto 명세서대로 늘려줌

--- src/main/java/com/team6/hanghaesisters/dto/post/PostDto.java
+++ src/main/java/com/team6/hanghaesisters/dto/post/PostDto.java
@@ -19,10 +19,12 @@ public class PostDto {
         }
     }
 
-    public record DetailResDto(Long id, String title, String content, String username) {
+    public record DetailResDto(Long id, String user, String title, String category, String imageBefore, String imageAfter, String content,
+                               int price, String hospitalAddress, String doctor) {
 
         public DetailResDto(Post post){
-            this(post.getId(), post.getTitle(), post.getContent(), post.getUser().getUsername());
+            this(post.getId(), post.getUser().getUsername(), post.getTitle(), post.getCategory(), post.getImageBefore(),
+                    post.getImageAfter(), post.getContent(), post.getPrice(), post.getHospitalAddress(), post.getDoctor());
         }
 
     }
